ffe2c26a3f60dff459a7170f7196de6a0f66bb10
madhanpandiyan <78121973+madhanpandiyan@users.noreply.github.com>
2021-09-01 02:24:21 +0100

    0109Amendments

---
+++ js/jquery/privacymodal.js
@@ -0,0 +1,60 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myPrivacyModal");
+var modal3 = document.getElementById("myCookieModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myPrivacyBtn");
+var btn3 = document.getElementById("myCookieBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("privacyclose")[0];
+var span3 = document.getElementsByClassName("cookieclose")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+modal.style.display = "block";
+}
+
+btn2.onclick = function() {
+modal2.style.display = "block";
+}
+
+btn3.onclick = function() {
+modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+modal.style.display = "none";
+}
+
+
+span2.onclick = function() {
+modal2.style.display = "none";
+}
+
+span3.onclick = function() {
+modal3.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
+modal.style.display = "none";
+}
+}
+
+window.onclick = function(event) {
+if (event.target == modal2) {
+modal2.style.display = "none";
+}
+}
+
+window.onclick = function(event) {
+if (event.target == modal3) {
+modal3.style.display = "none";
+}
+}
